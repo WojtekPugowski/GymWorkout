2d0aa0d1209fa3a5fe3bc234910619c91cdfba06
Wojtek Pugowski <wojtek@pugowski.pl>
2023-09-18 19:21:21 +0200

    Added Serilog. Setting sinks: console, file.

--- GymWorkout.WebApi/Controllers/WeatherForecastController.cs
+++ GymWorkout.WebApi/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ namespace GymWorkout.WebApi.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("Weather Forecast executing...");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
