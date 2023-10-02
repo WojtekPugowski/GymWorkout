06176ccbca133b897525f8f690f0868e275fdc2c
Wojtek Pugowski <Wojtek@BootCamp>
2023-10-02 21:03:28 +0200

    Added properties to Coach i Participant classes

--- GymWorkout.Domain/Entities/Coach.cs
+++ GymWorkout.Domain/Entities/Coach.cs
@@ -4,8 +4,10 @@ namespace GymWorkout.Domain.Entities
 {
     public class Coach : DomainEntity
     {
+        public int CoachId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Surname { get; set; }
+        public int ParticipantId { get; set; }
         public List<Participant> Participants { get; set; } = new List<Participant>();
     }
 }
--- GymWorkout.Domain/Entities/Participant.cs
+++ GymWorkout.Domain/Entities/Participant.cs
@@ -4,12 +4,12 @@ namespace GymWorkout.Domain.Entities
 {
     public class Participant : DomainEntity
     {
+        public int ParticipantId { get; set; }
+        public int CoachId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Surname { get; set; }
         public int Weight { get; set; }
         public int Growth { get; set; }
-        public Coach? Coach { get; set; }
-        public int CoachId { get; set; }
         public List<TrainingDay> MyTrenings { get; set; } = new List<TrainingDay>();
 
     }
